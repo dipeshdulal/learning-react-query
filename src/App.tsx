d37549a7aa6797501a199b612a0107d5f7a9d3a2
Dipesh Dulal <nilkantha.dipesh@gmail.com>
2020-09-27 19:45:37 +0545

    feat: add global query cache provider

--- src/App.tsx
+++ src/App.tsx
@@ -6,24 +6,29 @@ import { Pagination } from "./pages/Pagination";
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 import { InfiniteLoading } from "./pages/InfiniteLoading";
 import { DataMutation } from "./pages/DataMutation";
+import { QueryCache, ReactQueryCacheProvider } from "react-query";
+
+const qc = new QueryCache();
 
 function App() {
   return (
-    <div className="App">
-      <Router>
-        <Link to="/call-component">Call Component</Link>
-        <Link to="/">Pagination</Link>
-        <Link to="/infinite">Infinite Loading</Link>
-        <Link to="/data-mutation">Data Mutation</Link>
-        <Switch>
-          <Route exact path="/call-component" component={CallComponent} />
-          <Route exact path="/infinite" component={InfiniteLoading} />
-          <Route exact path="/data-mutation" component={DataMutation} />
-          <Route exact path="/" component={Pagination} />
-        </Switch>
-      </Router>
-      <ReactQueryDevtools />
-    </div>
+    <ReactQueryCacheProvider queryCache={qc}>
+      <div className="App">
+        <Router>
+          <Link to="/call-component">Call Component</Link>
+          <Link to="/">Pagination</Link>
+          <Link to="/infinite">Infinite Loading</Link>
+          <Link to="/data-mutation">Data Mutation</Link>
+          <Switch>
+            <Route exact path="/call-component" component={CallComponent} />
+            <Route exact path="/infinite" component={InfiniteLoading} />
+            <Route exact path="/data-mutation" component={DataMutation} />
+            <Route exact path="/" component={Pagination} />
+          </Switch>
+        </Router>
+        <ReactQueryDevtools />
+      </div>
+    </ReactQueryCacheProvider>
   );
 }
 
