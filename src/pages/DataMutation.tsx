8157f7805d1c65380d7fc16afd5cea87bd196e9c
Dipesh Dulal <nilkantha.dipesh@gmail.com>
2020-09-27 19:32:32 +0545

    feat: use mutation added

--- src/pages/DataMutation.tsx
+++ src/pages/DataMutation.tsx
@@ -1,17 +1,34 @@
 import React from "react";
 import { useForm } from "react-hook-form";
+import { useMutation, MutationFunction } from "react-query";
 
 interface FormData {
   text: string;
   name: string;
 }
 
+const createPosts: MutationFunction<any, any> = async (v) => {
+  const data = await fetch(`https://jsonplaceholder.typicode.com/posts`, {
+    method: "POST",
+    body: JSON.stringify(v),
+  });
+
+  return await data.json();
+};
+
 export const DataMutation = () => {
-  const { register, getValues, handleSubmit } = useForm<FormData>();
+  const { register, getValues, handleSubmit } = useForm<any>();
+
+  const [mutate, { data, error, isLoading }] = useMutation<any>(createPosts);
   return (
     <>
-      <form onSubmit={handleSubmit(console.log)}>
-        {JSON.stringify(getValues())}
+      <p>
+        {JSON.stringify(data)}
+        {JSON.stringify(error)}
+      </p>
+      {isLoading && <span>loading...</span>}
+      <form onSubmit={handleSubmit((v) => mutate(v))}>
+        <p>{JSON.stringify(getValues())}</p>
         <input
           type="text"
           placeholder="Post Text"
